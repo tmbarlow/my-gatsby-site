86a8cec4f8ac672fcb3950a75f30bbed0660c19f
Tyler Barlow <tmbarlow@gmail.com>
2021-12-06 12:59:42 -0500

    tried to finish tutorial 5 but had some issues with frontmatter date so just going to commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,18 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
+    title: "My Super Cool Blog",
   },
-  // In your gatsby-config.js
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/blog`,
-      }
-    }
-  ]
-};
+        path: `${__dirname}/blog/`,
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
+}
